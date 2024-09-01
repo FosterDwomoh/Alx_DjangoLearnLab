61a21b923531c76e258dc2903a9cb42142e7764a
Foster <kdwomoh0269@gmail.com>
2024-09-01 10:07:21 +0000

    api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -13,3 +13,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# create a router and register our viewset with it.
+router =DefaultRouter()
+router.register(r'books', BookViewSet)
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,3 +6,11 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(Viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
