1e0e0043e638e79db761e37425d75f8a0c24eb66
Foster <kdwomoh0269@gmail.com>
2024-09-01 09:55:47 +0000

    api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -11,5 +11,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api.urls/', include("api.urls")),
+    path('api/', include('api.urls')),
 ]
