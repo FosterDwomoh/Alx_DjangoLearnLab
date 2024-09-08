f45ac68b566553ce7c66c68091684d16795f0770
Foster <kdwomoh0269@gmail.com>
2024-09-08 21:55:37 +0000

    advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import status
 from rest_framework.response import Response 
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 class BookListView(generics.ListAPIView):
