f2e1dbf3bc1f0c2edb9311b2220b1e69d3851679
Foster <kdwomoh0269@gmail.com>
2024-09-15 20:35:52 +0000

    django_blog

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.urls import path, include
 
 urlpatterns = [
+    path('login/', views.LoginView.as_view(), name = 'login'),
+    path('logout/', views.LogoutView.as_view(), name = 'logout'),
+    path('register/', views.register, name = 'register'),
+    path('profile/', views.profile, name='profile'),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
