d4f23ab17c80e37eb04ba26b8fedd1ff20547575
Foster <kdwomoh0269@gmail.com>
2024-09-15 11:43:21 +0000

    django_blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method =='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'forms': form})
+
+    @login_required
+    def profile(request):
+        if request.method == 'POST':
+            user = request.userform = CustomUserCreationForm(request.POST, instance=user)
+            if form.is_valid():
+                form.save()
+                return redirect('profile')
+        else:
+            form = CustomUserCreationForm(instance=request.user)
+            return render(request, 'registration/profile.html', {'form': form})
