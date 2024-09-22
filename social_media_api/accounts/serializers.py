0fbc2a5c1abb6d382ac0e3fc7d372b0de4f6a8b9
Foster <kdwomoh0269@gmail.com>
2024-09-22 13:18:22 +0000

    social_media_api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import CustomUser
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'email', 'bio', 'profile_picture')
