d2ee89ccad1ac3acb375e3430060629da9c6f68a
Foster <kdwomoh0269@gmail.com>
2024-09-22 18:58:06 +0000

    accounts

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,6 +3,8 @@ from .views import RegisterView, LoginView, UserProfileView
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
+from .views import UserViewSet
+from .views import follow_user, unfollow_user 
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -11,4 +13,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('profile/<int:pk>/', UserProfileView.as_view(), name='profile'),
     path('', include(router.urls)),
+    path('follow/<int:user_id/', follow_user, name='follow-user'),
+    path('unfollow/<int:user_id/', unfollowfollow_user, name='unfollow'),
 ]
