25e6dca13df2470196b375cad2670953ed1988b3
Foster <kdwomoh0269@gmail.com>
2024-09-22 16:24:14 +0000

    accounts

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,3 +6,11 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+
+class CustomUser(AbstractUser):
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers'
+    )
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,10 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken  
 from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action 
+from .models import CustomUser
+from .serializer import UserSerializer
 
 
 get_user_model().objects.create_user
@@ -24,3 +28,20 @@ class LoginView(ObtainAuthToken):
 class UserProfileView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permissions_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['post'], url_path='follow')
+    def follow_user(self, request, pk=None):
+        user_to_follow = self.get_object()
+        request.user.following.add(user_to_follow)
+        return Response({'status': 'followed'})
+    
+     @action(detail=True, methods=['post'], url_path='unfollow')
+    def unfollowfollow_user(self, request, pk=None):
+        user_to_follow = self.get_object()
+        request.user.following.add(user_to_unfollow)
+        return Response({'status': 'unfollowed'})
